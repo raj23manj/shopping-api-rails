78ff484f42ba1ee69a3867bf99a39efee4b572d5
rajesh23-drg <rmanjunath@teamdrg.com>
2019-12-11 08:40:59 +0530

    Changed Logic To Meet Requirement

--- app/controllers/api/v1/service/product_discount_service.rb
+++ app/controllers/api/v1/service/product_discount_service.rb
@@ -14,30 +14,18 @@ module Api
           cart_items.each do |item|
             # get the rule suitable for current discount
             # if multiple rules are there for one product get the current sutible one
-            # p_discount_rule = (rules.select {|rule| (item.p_id == rule.product_id) && (rule.qty <= item.c_qty) })
-            #                   .sort {|a,b| b.qty <=> a.qty }.first   
-            
             p_discount_rules = (rules.select {|rule| (item.p_id == rule.product_id) && (rule.qty <= item.c_qty) })
-                              .sort {|a,b| b.qty <=> a.qty }  
-                     
+                              .sort {|a,b| a.qty <=> b.qty }         
             # calculate discount
-            # calculate_discount = p_discount_rule.present? ? calculate_actual_discount(p_discount_rule, item.c_qty, item.p_price) : 0
-            #calculate_discount = p_discount_rule.present? ?  : 0
             calculate_discount = if(p_discount_rules.present?)
                                     best_discount = calculate_best_discount(p_discount_rules, item.c_qty, item.p_price)
-                                    rule_discount_applied = p_discount_rules.select {|rule| rule.id == best_discount.keys.first}.first
-                                    best_discount.values.first
                                  else
-                                    rule_discount_applied = nil
                                     0
                                  end    
             
-            
             actual_price = (item.c_qty * item.p_price)
-            # items_with_discount << create_discounted_product(item, p_discount_rule, actual_price, calculate_discount)
-            items_with_discount << create_discounted_product(item, rule_discount_applied, actual_price, calculate_discount)
-            
-            if (rule_discount_applied.present? && (item.c_qty >= rule_discount_applied.qty))
+            items_with_discount << create_discounted_product(item, actual_price, calculate_discount)            
+            if(p_discount_rules.present?)
               total_price += calculate_discount #add discounted price
             else
               total_price += actual_price # add actual price
@@ -47,30 +35,88 @@ module Api
           { calculated_cart_details: items_with_discount, total_price: total_price } 
         end 
         
-        def calculate_best_discount(rules, item_qty, item_price)
-          all_rule_discounted = []
+        def calculate_best_discount(rules, item_qty, item_price)        
+          min_calculated_total = 0
+          
           rules.each do |rule| 
-            all_rule_discounted << calculate_actual_discount(rule, item_qty, item_price)
+            obj = calculate_actual_discount(rule, item_qty)
+            
+            if(obj[:remaninig_qty] != 0)
+              reamining_rules = rules.select {|rule| (rule.qty <= obj[:remaninig_qty]) }
+              total = calculate_discount_remianing(rules, obj[:remaninig_qty], item_price, obj[:price]) 
+              min_calculated_total = total if (total < min_calculated_total) || (min_calculated_total == 0)
+            end  
+            
+            if(obj[:remaninig_qty] == 0) || (min_calculated_total == 0)
+              min_calculated_total = obj[:price]
+            end  
           end
           
-          all_rule_discounted.sort{|a,b| a.values.first <=> b.values.first}.first  
-        end  
+          min_calculated_total
+        end
         
-        def calculate_actual_discount(rule, qty, actual_price)
+        
+        def calculate_discount_remianing(rules, item_qty, item_price, current_total)
+          min_discount_rule = rules.first
+          total_discount = 0
+        
+          while(item_qty >= min_discount_rule.qty) do
+            all_rule_discounted = []
+            rules.each do |rule| 
+              all_rule_discounted << calculate_actual_discount(rule, item_qty)
+            end
+                
+            best_rule_discount = best_total_discount(all_rule_discounted, current_total)
+            current_total = best_rule_discount[:total]
+            item_qty = best_rule_discount[:remaninig_qty] 
+            rules = rules.select {|rule| (rule.qty <= item_qty) }  
+          end
+          current_total + (item_qty * item_price)
+        end
+        
+        def best_total_discount(arr, prev_total)
+          min_total = {remaninig_qty: 0, total: 0}
+          zero_qty = {remaninig_qty: 0, total: 0}
+          arr.each do |obj|
+            # if 0 qty give preference
+            if((obj[:remaninig_qty] == 0) &&
+               ( zero_qty[:total] == 0|| obj[:price] < zero_qty[:total]))
+              zero_qty[:total] = obj[:price]
+              zero_qty[:remaninig_qty] = obj[:remaninig_qty]
+            end  
+            # find the min total
+            if(min_total[:total] == 0) || (obj[:price] < min_total[:total])
+              min_total[:total] = obj[:price]
+              min_total[:remaninig_qty] = obj[:remaninig_qty]
+            end   
+          end 
+        
+          if(zero_qty[:total] == 0)
+            min_total[:total] = min_total[:total] + prev_total
+          else
+            min_total[:total] = zero_qty[:total] + prev_total
+            min_total[:remaninig_qty] = zero_qty[:remaninig_qty]
+          end 
+        
+          min_total   
+        end    
+        
+        def calculate_actual_discount(rule, qty)
           discounted_qty = ((qty/rule.qty).to_s.split(".").first).to_i
-          discounted_price = (rule.discount_price * discounted_qty ) + (actual_price * (qty - (discounted_qty * rule.qty)))
-          { rule.id => discounted_price }
+          discounted_price = (rule.discount_price * discounted_qty ) #+ (actual_price * (qty - (discounted_qty * rule.qty)))
+          remaninig_qty = qty - (discounted_qty * rule.qty)
+          {price: discounted_price, remaninig_qty: remaninig_qty}
         end   
         
-        def create_discounted_product(item, p_discount_rule, actual_price, calculate_discount)
+        def create_discounted_product(item, actual_price, calculate_discount)
           OpenStruct.new(cart_id: item.c_cart_id,
                                                 cart_qty: item.c_qty,
                                                 product_name: item.p_name,
                                                 actual_product_price: item.p_price,
                                                 actual_total: actual_price,
-                                                discounted_total: (p_discount_rule.blank? ? 0 : calculate_discount),
-                                                actual_discount_price: (p_discount_rule.blank? ? 0 : p_discount_rule.discount_price),
-                                                actual_discount_qty: (p_discount_rule.blank? ? 0 : p_discount_rule.qty)
+                                                discounted_total: ((calculate_discount == 0) ? 0 : calculate_discount)
+                                                # actual_discount_price: (p_discount_rule.blank? ? 0 : p_discount_rule.discount_price),
+                                                # actual_discount_qty: (p_discount_rule.blank? ? 0 : p_discount_rule.qty)
                                               ).marshal_dump
         end   
         
--- spec/controllers/api/v1/service/product_discount_service_spec.rb
+++ spec/controllers/api/v1/service/product_discount_service_spec.rb
@@ -15,42 +15,42 @@ RSpec.describe Api::V1::Service::ProductDiscountService, type: :service do
     DiscountRule.delete_all
   end
   
-  describe "discount_on_products method with one rule per product" do
-  
-    before do
-      # Product Discount Rules
-      @discount_rule1 = create(:discount_rule, product_id: product1.id, qty: 3, discount_price: 75)  # "A"
-      @discount_rule2 = create(:discount_rule, product_id: product2.id, qty: 2, discount_price: 35)  # "B"
-      @product_discount_service = Api::V1::Service::ProductDiscountService.new(DiscountRule.all.to_a)
-      @discount_service =  Api::V1::Service::DiscountService.new()
-      @cart_service = Api::V1::Service::CartService.new(@discount_service) 
-    end
-  
-    context "Return expected Result" do
-      before do
-        @cart_detail1 = create(:cart_detail, product_id: product1.id, cart_id: cart.id, qty: 1) 
-        @cart_detail2 = create(:cart_detail, product_id: product2.id, cart_id: cart.id, qty: 1) 
-        @cart_detail3 = create(:cart_detail, product_id: product3.id, cart_id: cart.id, qty: 1)
-        @items = @cart_service.cart_detail_with_product(cart.id)
-  
-        @response = @product_discount_service.discount_on_products(@items)  
-      end
-  
-      it "run total_price" do
-        expect(@response[:total_price]).to eq(100)
-      end
-    end
-  
-    context "Return expected Result" do
-      before do
-        @response = @product_discount_service.calculate_actual_discount(@discount_rule1, 5, 30)  
-      end
-  
-      it "run total_price" do
-        expect(@response).to eq({@discount_rule1.id => 135})
-      end
-    end
-  end
+  # describe "discount_on_products method with one rule per product" do
+  # 
+  #   before do
+  #     # Product Discount Rules
+  #     @discount_rule1 = create(:discount_rule, product_id: product1.id, qty: 3, discount_price: 75)  # "A"
+  #     @discount_rule2 = create(:discount_rule, product_id: product2.id, qty: 2, discount_price: 35)  # "B"
+  #     @product_discount_service = Api::V1::Service::ProductDiscountService.new(DiscountRule.all.to_a)
+  #     @discount_service =  Api::V1::Service::DiscountService.new()
+  #     @cart_service = Api::V1::Service::CartService.new(@discount_service) 
+  #   end
+  # 
+  #   context "Return expected Result" do
+  #     before do
+  #       @cart_detail1 = create(:cart_detail, product_id: product1.id, cart_id: cart.id, qty: 1) 
+  #       @cart_detail2 = create(:cart_detail, product_id: product2.id, cart_id: cart.id, qty: 1) 
+  #       @cart_detail3 = create(:cart_detail, product_id: product3.id, cart_id: cart.id, qty: 1)
+  #       @items = @cart_service.cart_detail_with_product(cart.id)
+  # 
+  #       @response = @product_discount_service.discount_on_products(@items)  
+  #     end
+  # 
+  #     it "run total_price" do
+  #       expect(@response[:total_price]).to eq(100)
+  #     end
+  #   end
+  # 
+  #   context "Return expected Result" do
+  #     before do
+  #       @response = @product_discount_service.calculate_actual_discount(@discount_rule1, 5, 30)  
+  #     end
+  # 
+  #     it "run total_price" do
+  #       expect(@response).to eq({@discount_rule1.id => 135})
+  #     end
+  #   end
+  # end
   
   describe "discount_on_products method with multiple rules per product" do
     before do
@@ -70,11 +70,11 @@ RSpec.describe Api::V1::Service::ProductDiscountService, type: :service do
         @cart_detail = create(:cart_detail, product_id: product1.id, cart_id: cart.id, qty: 10) 
         @items = @cart_service.cart_detail_with_product(cart.id)
         @response = @product_discount_service.calculate_best_discount(DiscountRule.where("product_id = ?", product1.id).to_a, 
-                                                                      @cart_detail.qty, product1.price)  
+                                                                      @cart_detail.qty, 30)  
       end
-  
+    
       it "run total_price" do
-        expect(@response).to eq({@discount_rule2.id => 225})
+        expect(@response).to eq(225)
       end
     end
   
@@ -83,11 +83,11 @@ RSpec.describe Api::V1::Service::ProductDiscountService, type: :service do
         @cart_detail = create(:cart_detail, product_id: product1.id, cart_id: cart.id, qty: 11) 
         @items = @cart_service.cart_detail_with_product(cart.id)
         @response = @product_discount_service.calculate_best_discount(DiscountRule.where("product_id = ?", product1.id).to_a, 
-                                                                      @cart_detail.qty, product1.price)  
+                                                                      @cart_detail.qty, 30)  
       end
-
+    
       it "run total_price" do
-        expect(@response).to eq({@discount_rule2.id => 255})
+        expect(@response).to eq(245)
       end
     end
   end
